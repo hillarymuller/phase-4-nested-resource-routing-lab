ccc1bd416475f918b8c1bd858007e3a3207d8df3
Hillary Muller <hillaryaward@gmail.com>
2022-10-03 09:34:39 -0500

    commit tree 7cbec1dc5a82f45e11a1e1c230dfe54d5d8f8242

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
 
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
   end
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+    else
+      item = Item.find(params[:id])
+    end
+    render json: item, include: :user
+  end
+  def create
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      render json: item, status: :created
+  end
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
 
 end
